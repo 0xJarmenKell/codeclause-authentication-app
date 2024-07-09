1c8e58bdea1478c1ff7ee10684544c13458f9b53
Youssef Y. Younis <87781261+0xJarmenKell@users.noreply.github.com>
2024-07-09 22:37:41 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "YourAPI",
+  apiKey: "AIzaSyDLA8Mr_STDPQ_6gA0nXvgLU79e-HzBvlg",
   authDomain: "codeclause-auth.firebaseapp.com",
   projectId: "codeclause-auth",
   storageBucket: "codeclause-auth.appspot.com",
   messagingSenderId: "384921590703",
-  appId: "YourID"
+  appId: "1:384921590703:web:38d0d9aa49c135646f2d5a"
 };
 
 
